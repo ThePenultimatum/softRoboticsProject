21b9c9d62ddcd71db92b5c7ab7f36e215277c551
Mark Dyehouse <markdyehouse2019@u.northwestern.edu>
2019-06-06 13:00:43 -0500

    Added demo code

--- src/main.c
+++ src/main.c
@@ -59,18 +59,130 @@ void setup_nu32_softrobotics() {
   valveToState(VALVE19, VCLOSED);
 }
 
+void delayRough() {
+  _CP0_SET_COUNT(0);
+  while (_CP0_GET_COUNT() < 240000000) {
+    Nop();
+  } 
+}
+
+void cyclePumps() {
+  pumpToState(PUMP0, PUMPON);
+  delayRough();
+  pumpToState(PUMP1, PUMPON);
+  delayRough();
+  pumpToState(PUMP2, PUMPON);
+  delayRough();
+  pumpToState(PUMP3, PUMPON);
+  delayRough();
+  pumpToState(PUMP4, PUMPON);
+  delayRough();
+  pumpToState(PUMP0, PUMPOFF);
+  delayRough();
+  pumpToState(PUMP1, PUMPOFF);
+  delayRough();
+  pumpToState(PUMP2, PUMPOFF);
+  delayRough();
+  pumpToState(PUMP3, PUMPOFF);
+  delayRough();
+  pumpToState(PUMP4, PUMPOFF);
+  delayRough();
+}
+
+void cycleValves() {
+  valveToState(VALVE0, VOPEN);
+  delayRough();
+  valveToState(VALVE0, VCLOSED);
+  delayRough();
+  valveToState(VALVE1, VOPEN);
+  delayRough();
+  valveToState(VALVE1, VCLOSED);
+  delayRough();
+  valveToState(VALVE2, VOPEN);
+  delayRough();
+  valveToState(VALVE2, VCLOSED);
+  delayRough();
+  valveToState(VALVE3, VOPEN);
+  delayRough();
+  valveToState(VALVE3, VCLOSED);
+  delayRough();
+  valveToState(VALVE4, VOPEN);
+  delayRough();
+  valveToState(VALVE4, VCLOSED);
+  delayRough();
+  valveToState(VALVE5, VOPEN);
+  delayRough();
+  valveToState(VALVE5, VCLOSED);
+  delayRough();
+  valveToState(VALVE6, VOPEN);
+  delayRough();
+  valveToState(VALVE6, VCLOSED);
+  delayRough();
+  valveToState(VALVE7, VOPEN);
+  delayRough();
+  valveToState(VALVE7, VCLOSED);
+  delayRough();
+  valveToState(VALVE8, VOPEN);
+  delayRough();
+  valveToState(VALVE8, VCLOSED);
+  delayRough();
+  valveToState(VALVE9, VOPEN);
+  delayRough();
+  valveToState(VALVE9, VCLOSED);
+  delayRough();
+  valveToState(VALVE10, VOPEN);
+  delayRough();
+  valveToState(VALVE10, VCLOSED);
+  delayRough();
+  valveToState(VALVE11, VOPEN);
+  delayRough();
+  valveToState(VALVE11, VCLOSED);
+  delayRough();
+  valveToState(VALVE12, VOPEN);
+  delayRough();
+  valveToState(VALVE12, VCLOSED);
+  delayRough();
+  valveToState(VALVE13, VOPEN);
+  delayRough();
+  valveToState(VALVE13, VCLOSED);
+  delayRough();
+  valveToState(VALVE14, VOPEN);
+  delayRough();
+  valveToState(VALVE14, VCLOSED);
+  delayRough();
+  valveToState(VALVE15, VOPEN);
+  delayRough();
+  valveToState(VALVE15, VCLOSED);
+  delayRough();
+  valveToState(VALVE16, VOPEN);
+  delayRough();
+  valveToState(VALVE16, VCLOSED);
+  delayRough();
+  valveToState(VALVE17, VOPEN);
+  delayRough();
+  valveToState(VALVE17, VCLOSED);
+  delayRough();
+  valveToState(VALVE18, VOPEN);
+  delayRough();
+  valveToState(VALVE18, VCLOSED);
+  delayRough();
+  valveToState(VALVE19, VOPEN);
+  delayRough();
+  valveToState(VALVE19, VCLOSED);
+  delayRough();
+}
 
 int32_t main(void) {
   setup_nu32_softrobotics();
 
-  unsigned int i = 0, data = 0;
-  unsigned int buflen = 20;
-  char buffer[buflen];
+  unsigned int i = 0;//, data = 0;
+  //unsigned int buflen = 20;
+  //char buffer[buflen];
 
-  float midval = 1.8;
+  //float midval = 1.8;
 
-  int valvesetInUse = VALVESET0;
-  int pumpInUse = PUMP0;
+  //int valvesetInUse = VALVESET0;
+  //int pumpInUse = PUMP0;
 
   /*_CP0_SET_COUNT(0);
   while (_CP0_GET_COUNT() < 80000000) {
@@ -80,26 +192,38 @@ int32_t main(void) {
   //valveToState(VALVE0, VOPEN);
   //switchState(VALVESAIROPEN, VALVESET0);
   while (1) {
-    NU32_ReadUART3(buffer, buflen);
-    sscanf(buffer, "%6.4f             ", data);
 
-    if (data <= midval) {
-      switchState(VALVESAIROPEN, valvesetInUse); // BACKWARDS SETTINGS BECAUSE OF MISLABELING WITH MANIFOLD
-    } else {
-      switchState(VALVESVACUUMOPEN, valvesetInUse); // BACKWARDS SETTINGS BECAUSE OF MISLABELING WITH MANIFOLD
-    }
-
-    pumpToState(pumpInUse, PUMPON);
-
-    _CP0_SET_COUNT(0);
-    while (_CP0_GET_COUNT() < 4000000) {
-      Nop();
-    }
-
-    pumpToState(pumpInUse, PUMPOFF);
-    allPowerOff();
+    cyclePumps();
+    cycleValves();
 
 
+    //switchState(VALVESET0, VALVESAIROPEN);
+    //switchState(VALVESET2, VALVESAIROPEN);
+    //pumpToState(PUMP0, PUMPON);
+    //delayRough();
+    //pumpToState(PUMP0, PUMPOFF);
+    //switchState(VALVESET0, VALVESCLOSED);
+    //pumpToState(PUMP2, PUMPON);
+    //delayRough();
+    //pumpToState(PUMP2, PUMPOFF);
+    //switchState(VALVESET2, VALVESCLOSED);
+    //switchState(VALVESET0, VALVESVACUUMOPEN);
+    //pumpToState(PUMP0, PUMPON);
+    //delayRough();
+    //pumpToState(PUMP0, PUMPOFF);
+    //switchState(VALVESET0, VALVESCLOSED);
+    //switchState(VALVESET2, VALVESVACUUMOPEN);
+    //pumpToState(PUMP2, PUMPON);
+    //delayRough();
+    //pumpToState(PUMP2, PUMPOFF);
+    //switchState(VALVESET2, VALVESCLOSED);
+    //switchState(VALVESET0, VALVESOPEN);
+    //switchState(VALVESET2, VALVESOPEN);
+    //delayRough();
+    //switchState(VALVESET0, VALVESCLOSED);
+    //switchState(VALVESET2, VALVESCLOSED);
+    //allPowerOff();
+    //delayRough();
   }
   return 0;
 }
